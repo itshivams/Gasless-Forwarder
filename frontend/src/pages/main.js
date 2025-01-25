1b352ab8750d39cc6c63f9ef8840c4ffa4e75633
ritikgupta06 <onlyritik10@gmail.com>
2025-01-25 15:52:18 +0530

    add all pages

--- frontend/src/pages/main.js
+++ frontend/src/pages/main.js
@@ -3,7 +3,6 @@ import TransactionForm from "../components/TransactionForm";
 export default function Main() {
   return (
     <div style={{ padding: "50px", textAlign: "center" }}>
-      <h1>Gasless</h1>
       <TransactionForm />
     </div>
   );
